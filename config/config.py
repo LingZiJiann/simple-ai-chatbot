a43a85f268c3f33e8189b1b1138c1c27678ef411
Ling Zi Jian <lingzijian@hotmail.com>
2026-05-19 22:12:12 +0800

    feat: added config

---
+++ config/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=Path(__file__).parent.parent / ".env")
+
+
+settings = Settings()
